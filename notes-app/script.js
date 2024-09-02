923cd6a87f55430d91e0d240969cc68b1f0e851f
Rushel <ruselaq@gmail.com>
2024-09-02 15:00:14 +0800

    Using Tailwind CSS

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,7 +1,7 @@
-const createBtn = document.querySelector(".btn");
-const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.getElementById("btn");
+const notesContainer = document.getElementById("notes-container");
 
-let notes = document.querySelectorAll(".input-box");
+let notes = document.querySelectorAll("#input-box");
 
 function showNotes() {
     notesContainer.innerHTML = localStorage.getItem("notes");
@@ -14,11 +14,15 @@ function updateStorage() {
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
-    inputBox.className = "input-box";
+    inputBox.id = "input-box";
+    inputBox.className = "bg-white text-gray-800 rounded-md p-5 relative min-h-[150px] max-w-[500px] mx-0 mb-5 outline-none";
     inputBox.setAttribute("contenteditable", "true");
+
     img.src = "images/delete.png";
+    img.className = "w-6 absolute bottom-4 right-4 cursor-pointer";
     notesContainer.appendChild(inputBox).appendChild(img);
     updateStorage();
+    setupNotes();
 });
 
 notesContainer.addEventListener("click", (e) => {
@@ -27,7 +31,7 @@ notesContainer.addEventListener("click", (e) => {
         updateStorage();
     }
     else if (e.target.tagName === "P") {
-        notes = document.querySelectorAll(".input-box");
+        notes = document.querySelectorAll("#input-box");
         notes.forEach(note => {
             note.onkeyup = () => {
                 updateStorage();
