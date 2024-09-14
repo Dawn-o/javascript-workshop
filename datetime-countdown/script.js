fb194034c4c3a335f4f3104ff5e5ca1e179b1c1c
Rushel <ruselaq@gmail.com>
2024-09-14 13:26:18 +0800

    Datetime Countdown

---
+++ datetime-countdown/script.js
@@ -0,0 +1,24 @@
+let countDownDate = new Date("Jan 01, 2025 00:00:00").getTime();
+let x = setInterval(() => {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+
+}, 1000);
