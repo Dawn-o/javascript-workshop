aad160c9b692d6630e01b0834f78c634fcba3f66
Rushel <ruselaq@gmail.com>
2024-09-14 11:33:51 +0800

    Improve password strength meter logic in JavaScript Code

--- password-strength-indicator/script.js
+++ password-strength-indicator/script.js
@@ -5,22 +5,19 @@ const str = document.getElementById("strength");
 pass.addEventListener("input", () => {
     const length = pass.value.length;
 
-    if (length > 0) {
-        msg.style.display = "block";
-    } else {
-        msg.style.display = "none";
-    }
+    msg.style.display = length > 0 ? "block" : "none";
+
     if (length < 4) {
-        str.innerHTML = "weak";
-        pass.style.borderColor = "#ff5925";
-        msg.style.color = "#ff5925";
-    } else if (length >= 4 && length < 8) {
-        str.innerHTML = "medium";
-        pass.style.borderColor = "yellow";
-        msg.style.color = "yellow";
-    } else if (length <= 8) {
-        str.innerHTML = "strong";
-        pass.style.borderColor = "#26d730";
-        msg.style.color = "#26d730";
+        Strength("weak", "#ff5925");
+    } else if (length < 8) {
+        Strength("medium", "yellow");
+    } else {
+        Strength("strong", "#26d730");
     }
-})
+});
+
+function Strength(text, color) {
+    str.innerHTML = text;
+    pass.style.borderColor = color;
+    msg.style.color = color;
+}
