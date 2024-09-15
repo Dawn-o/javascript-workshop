a80ee3cdccb1a996d270ea9730e126606a00dd87
Rushel <ruselaq@gmail.com>
2024-09-16 06:37:48 +0800

    Add dynamic project links using JavaScript

---
+++ script.js
@@ -0,0 +1,34 @@
+const projects = [
+    "age-calculator-app",
+    "calculator-app",
+    "datetime-countdown",
+    "form-validation",
+    "h-scroll-slider",
+    "music-player-app",
+    "notes-app",
+    "password-generator-app",
+    "password-strength-indicator",
+    "popup",
+    "qr-generator-app",
+    "quiz-app",
+    "stopwatch-app",
+    "text-to-speech",
+    "toast-notification",
+    "todo-list-app",
+    "toggle-password",
+    "weather-app",
+    "image-transition",
+    "mini-calendar-app",
+];
+
+const projectList = document.getElementById("project-list");
+
+projects.forEach((project) => {
+    const listItem = document.createElement("a");
+    listItem.href = project;
+    listItem.target = "_blank";
+    listItem.className =
+        "bg-transparent border-2 border-gray-900 outline-none py-3 px-4 text-gray-900 text-center flex items-center justify-center uppercase font-medium mt-8 cursor-pointer transition duration-300 ease-in-out hover:bg-gray-900 hover:text-white";
+    listItem.textContent = project.replace(/-/g, " "); // Convert kebab-case to normal text
+    projectList.appendChild(listItem);
+});
